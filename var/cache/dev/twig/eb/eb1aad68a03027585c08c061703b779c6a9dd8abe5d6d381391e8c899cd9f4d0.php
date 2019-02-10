a5c87fee36fbf8761108bb4ddf9d209759de1328
root <piotr@debian.debian>
2019-02-10 21:41:13 +0100

    first commit

---
+++ var/cache/dev/twig/eb/eb1aad68a03027585c08c061703b779c6a9dd8abe5d6d381391e8c899cd9f4d0.php
@@ -0,0 +1,62 @@
+<?php
+
+/* @Framework/Form/checkbox_widget.html.php */
+class __TwigTemplate_b39bd363a2d672c773f6fa3d859a922df652cdfb7c9b55047f17380458bb0b33 extends Twig_Template
+{
+    private $source;
+
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@Framework/Form/checkbox_widget.html.php"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@Framework/Form/checkbox_widget.html.php"));
+
+        // line 1
+        echo "<input type=\"checkbox\"
+    <?php echo \$view['form']->block(\$form, 'widget_attributes') ?>
+    <?php if (strlen(\$value) > 0): ?> value=\"<?php echo \$view->escape(\$value) ?>\"<?php endif ?>
+    <?php if (\$checked): ?> checked=\"checked\"<?php endif ?>
+/>
+";
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@Framework/Form/checkbox_widget.html.php";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  29 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<input type=\"checkbox\"
+    <?php echo \$view['form']->block(\$form, 'widget_attributes') ?>
+    <?php if (strlen(\$value) > 0): ?> value=\"<?php echo \$view->escape(\$value) ?>\"<?php endif ?>
+    <?php if (\$checked): ?> checked=\"checked\"<?php endif ?>
+/>
+", "@Framework/Form/checkbox_widget.html.php", "/application/vendor/symfony/framework-bundle/Resources/views/Form/checkbox_widget.html.php");
+    }
+}
